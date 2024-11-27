1aea33990333179dafa4f474389fddc9c53cc199
harish_selva <harishselvaraj27@gmail.com>
2024-11-27 12:35:42 +0530

    Revert "added sigup functionality"
    This reverts commit ac316c46d2b7a8a2b070c52214bb90c759aa6552.

--- server/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-const authRouter = require('./controller/authController');
-
-app.use(express.json());
-app.use('/api/auth/',authRouter);
-
-module.exports = app;
