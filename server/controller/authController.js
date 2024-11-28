0be2ca451d3944582037d0247964d5476f6691b0
harish_selva <harishselvaraj27@gmail.com>
2024-11-28 22:55:51 +0530

    login functionality added

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../model/user');
 const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken")
 
 router.post('/signup',async (req,res)=>{
     try{
@@ -27,6 +28,39 @@ router.post('/signup',async (req,res)=>{
             success: true
         })
 
+    }catch(err){
+        res.status(500).send({
+            message:err.message,
+            success: false
+        })
+    }
+});
+
+router.post('/login',async (req,res)=>{
+    try{
+        //1. check if user exists
+        const user = await User.findOne({email: req.body.email});
+        if(!user){
+            return res.status(400).json({
+                message: 'User does not exists',
+                status: false
+            })
+        }
+        //2. check password
+        const isvalid = await bcrypt.compare(req.body.password,user.password);
+        if(!isvalid){
+            return res.status(400).json({
+                message: "incorrect password",
+                status: false
+            })
+        }
+        //3. log in and give jwt
+        const token = await jwt.sign({userId: user._id},process.env.SECRET_KEY);
+        res.status(200).json({
+            message:"user login successful!",
+            status: true,
+            token: token
+        })
     }catch(err){
         res.status(500).send({
             message:err.message,
